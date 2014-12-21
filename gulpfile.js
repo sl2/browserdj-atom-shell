18a8be3e135e1df1ea929ae78f1b4c33e29fbc9c
sl2 <dimension.sf@gmail.com>
2014-12-21 22:14:35 +0900

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
 var gulp = require('gulp');
 var gulpAtom = require('gulp-atom');
+var zip = require('gulp-zip');
 
-gulp.task('atom', function() {
+var version = 'v0.20.1';
+var platforms = ['win32-ia32', 'darwin-x64', 'linux-x64'];
+
+// build only
+gulp.task('build', function() {
     return gulpAtom({
         srcPath: './src',
         releasePath: './release',
         cachePath: './cache',
-        version: 'v0.20.1',
+        version: version,
         rebuild: false,
-        platforms: ['win32-ia32', 'darwin-x64', 'linux-x64']
+        platforms: platforms
     });
+});
+
+// create zip
+gulp.task('default', ['build'], function() {
+    for (i in platforms) {
+        var os = platforms[i];
+        gulp.src('./release/'+version+'/'+os+'/*')
+            .pipe(zip('browserdj'+'_'+version+'_'+os+'.zip'))
+            .pipe(gulp.dest('./dist'));
+    }
 });
 
 
