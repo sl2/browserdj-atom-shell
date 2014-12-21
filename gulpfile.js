e75b9bcb9e9fe0a45df1138819f6096660341f35
sl2 <dimension.sf@gmail.com>
2014-12-22 03:29:06 +0900

    bug fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var gulpAtom = require('gulp-atom');
-var zip = require('gulp-zip');
+var tar = require('gulp-tar');
+var gzip = require('gulp-gzip');
 
 var version = 'v0.20.1';
-var platforms = ['win32-ia32', 'darwin-x64', 'linux-x64'];
+var platforms = ['darwin-x64', 'win32-ia32', 'linux-x64'];
 
 // build only
 gulp.task('build', function() {
@@ -21,9 +22,14 @@ gulp.task('build', function() {
 gulp.task('default', ['build'], function() {
     for (i in platforms) {
         var os = platforms[i];
-        gulp.src('./release/'+version+'/'+os+'/*')
-            .pipe(zip('browserdj'+'_'+version+'_'+os+'.zip'))
-            .pipe(gulp.dest('./dist'));
+        if (os != 'darwin-x64'){
+            var base = './release/'+version+'/'+os+'/**';
+            var tar_name = 'browserdj'+'_'+version+'_'+os+'.tar';
+            gulp.src(base)
+                .pipe(tar(tar_name))
+                .pipe(gzip())
+                .pipe(gulp.dest('./dist'));
+        }
     }
 });
 
